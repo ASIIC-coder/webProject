e8ccebbcd6d9db669057f9a425654859e66631d0
aricleo <l3326135@163.com>
2022-04-12 14:37:11 +0800

    添加了登录访问拦截器

---
+++ src/main/java/com/ahg/community/controller/interceptor/AlphaInterceptor.java
@@ -0,0 +1,32 @@
+package com.ahg.community.controller.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AlphaInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(AlphaInterceptor.class);
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        logger.debug("preHandle: " + handler.toString());
+        return true;
+    }
+
+    //在Controller之后运行
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView){
+        logger.debug("postHandle: " + handler.toString());
+    }
+
+    //在TemplateEngine之后执行
+    public void afterCompletion(HttpServletResponse request, HttpServletResponse response, Object handler, Exception ex){
+        logger.debug("afterCompletion: " + handler.toString());
+    }
+
+}
